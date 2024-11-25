f0a358a2829e8e577d97f48373f331a740945dca
natylouise <natylouise12@gmail.com>
2024-11-25 18:06:50 -0300

    Formulario inserido no cadastro

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+ 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrls: ['./cadastro.component.css'],
 })
+ 
 export class CadastroComponent {
-
+  nomeCompleto = '';
+  nomeSocial = '';
+  nomeMae = '';
+  nomePai = '';
+  dataNascimento = '';
+  sexo = '';
+  nacionalidade = '';
+  municipioNascimento = '';
+  racaCor = '';
+  frequentaEscola = '';
+  contatoCelular = '';
+  contatoResidencial = '';
+  contatoEmail = '';
 }
